d9b43f858050d023dcc0334740ac914580392511
lishijia <shijia.li@hotmail.com>
2018-09-25 17:15:40 +0800

    java 客户端连接 hive thrift接口服务查询数据

---
+++ hive-data-demo/src/main/java/com/jiarong/data/hive/ThirftHiveDemo.java
@@ -0,0 +1,68 @@
+package com.jiarong.data.hive;
+
+import java.sql.*;
+
+/**
+ * @program:
+ * @description:
+ * @author: lishijia
+ * @create: 2018/9/25 16:37
+ **/
+public class ThirftHiveDemo {
+
+    String driverClass = "org.apache.hive.jdbc.HiveDriver";
+    String url = "jdbc:hive2://hadoop100:10000/default";
+
+    public static void main(String[] args) {
+        long l = System.currentTimeMillis();
+        ThirftHiveDemo thirftHiveDemo = new ThirftHiveDemo();
+        thirftHiveDemo.search();
+        System.out.println(System.currentTimeMillis()-l);
+    }
+
+    private void search(){
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            conn = getConn();
+            System.out.println(conn);
+            stmt = conn.createStatement();
+            // hive表名
+            String tableName = "st_order";
+            String sql = "select * from " + tableName + " limit 10";
+            System.out.println("Running:" + sql);
+            ResultSet res = stmt.executeQuery(sql);
+            System.out.println("执行 select * query 运行结果:");
+            while (res.next()) {
+                System.out.println(res.getString(1) + "\t" + res.getString(2));
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            System.exit(1);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.exit(1);
+        } finally {
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                    stmt = null;
+                }
+                if (conn != null) {
+                    conn.close();
+                    conn = null;
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private Connection getConn() throws ClassNotFoundException,
+            SQLException {
+        Class.forName(driverClass);
+        Connection conn = DriverManager.getConnection(url);
+        return conn;
+    }
+
+}
