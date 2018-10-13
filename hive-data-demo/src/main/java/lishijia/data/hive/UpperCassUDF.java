4ae16d7539beeb70893d88668ec5ee01f8a6a912
lishijia <shijia.li@hotmail.com>
2018-10-13 11:20:33 +0800

    demo data

---
+++ hive-data-demo/src/main/java/lishijia/data/hive/UpperCassUDF.java
@@ -0,0 +1,19 @@
+package lishijia.data.hive;
+
+import org.apache.hadoop.hive.ql.exec.UDF;
+
+/**
+ * @program:
+ * @description:
+ * @author: lishijia
+ * @create: 2018/10/13 11:06
+ **/
+public class UpperCassUDF extends UDF {
+    public String evaluate(String input) {
+        if (input == null) {
+            return null;
+        } else {
+            return new String(input.toUpperCase());
+        }
+    }
+}
