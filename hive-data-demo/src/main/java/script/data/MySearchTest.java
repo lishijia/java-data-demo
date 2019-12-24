30db73a554da565a1d74a1d3b324de9ef70b8c97
lishijia <john.li@nf-3.com>
2019-12-24 11:21:15 +0800

    test

--- hive-data-demo/src/main/java/script/data/MySearchTest.java
+++ hive-data-demo/src/main/java/script/data/MySearchTest.java
@@ -1,5 +1,6 @@
 package script.data;
 
+import com.alibaba.fastjson.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
@@ -9,7 +10,7 @@ import java.util.Map;
 
 public class MySearchTest {
     //private static String url = "http://rch.hfkktt.com/mp/article/index?_t=1542364074826";
-    private static String url = "http://rch.hfkktt.com/mp/api/articles?before=&categories=";
+    private static String url = "http://api.tongji.qinglizi.cn/app/list?pn=1&rn=20";
     private static String blogName = "guoxiaolongonly";
 
     public static void main(String[] args) {
@@ -25,23 +26,26 @@ public class MySearchTest {
     public static void getArticleListFromUrl(final String listurl) {
         Document doc = null;
         try {
-            String referrer = "http://rch.hfkktt.com/mp/article/index?_t=1542364074826";
+            String referrer = "http://www.qinglizi.cn/home/list";
             Map<String, String > cookies = new HashMap<String, String>();
-            cookies.put("viewerWxOpenId","oxHmT1SDJA_O3z-mtTyWiJQKzWJA");
-            cookies.put("viewerWxUnionId2","oLcWOv4E_G68ujA0zaYxlvjJeeig");
-            cookies.put("wxMpToken","3486706fa7f9af5550b2d3658e7fad9eb5331ab08688c6e392236fef5aac6627");
-            cookies.put("wxMpTokenForHomepage","3486706fa7f9af5550b2d3658e7fad9eb5331ab08688c6e392236fef5aac6627");
+            cookies.put("X-TOKEN","1c60ad1fb4248fd0f262f160664b4841");
+            cookies.put("Set-Cookie","SID=p43gq0j23ts4emlmkca2iovd74; path=/; HttpOnly");
             String ua = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36 MicroMessenger/6.5.2.501 NetType/WIFI WindowsWechat QBCore/3.43.901.400 QQBrowser/9.0.2524.400";
             doc = Jsoup.connect(listurl).userAgent(ua)
                     .referrer(referrer)
-                    .cookies(cookies)
+//                    .cookies(cookies)
+                    .header("X-TOKEN", "1c60ad1fb4248fd0f262f160664b4841")
+                    .header("Cookie", "SID=h0fr6siqghubq75d03o6ahi2d0; ")
                     .header("Content-Type", "application/json;charset=UTF-8")
                     .ignoreContentType(true)
                     .timeout(3000).post();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println(doc.body().toString());
+        String body = doc.body().html().toString().replace("&quot;", "\"");
+        JSONObject jsonObject = JSONObject.parseObject(body);
+        System.out.println(jsonObject.toJSONString());
+
     }
 
 }
